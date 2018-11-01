44df700d92c720685421f2c9bf406934bc670edf
xww <xww@8531.cn>
2018-11-02 00:03:02 +0800

    go in c

--- libethash/truego.h
+++ libethash/truego.h
@@ -38,8 +38,11 @@ typedef long long GoInt64;
 typedef unsigned long long GoUint64;
 typedef GoInt64 GoInt;
 typedef GoUint64 GoUint;
+typedef __SIZE_TYPE__ GoUintptr;
 typedef float GoFloat32;
 typedef double GoFloat64;
+typedef float _Complex GoComplex64;
+typedef double _Complex GoComplex128;
 
 /*
   static assertion to make sure the file is being used on architecture
